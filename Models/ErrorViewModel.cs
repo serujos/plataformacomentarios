a4e623f8cdd3d754f501c8b0f3d1c8b75c3eaf06
jos <josmerjauriacaceria@gmail.com>
2025-05-23 20:45:13 -0500

    proyecto base

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace plataformacomentarios.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
